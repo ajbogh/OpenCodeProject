aec0536ae13c37b4a14d753e1474da0b22d1c9e3
Allan Bogh <ajbogh@allanbogh.com>
2020-12-07 22:24:14 -0800

    Added node-image-farmer to pm2 config.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,24 +1,39 @@
 module.exports = {
-  apps : [{
-    name      : 'opencodeproject-server',
-    script    : 'server.js',
-    watch     : true,
-    env: {
-      NODE_ENV: 'development'
+  apps : [
+    {
+      name      : 'opencodeproject-server',
+      script    : 'server.js',
+      watch     : true,
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
     },
-    env_production : {
-      NODE_ENV: 'production'
-    }
-  },
-  {
-    name      : 'opencodeproject-webhooks',
-    script    : 'webhooks/webhook-server.js',
-    watch     : true,
-    env: {
-      NODE_ENV: 'development'
+    {
+      name      : 'opencodeproject-webhooks',
+      script    : 'webhooks/webhook-server.js',
+      watch     : true,
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
     },
-    env_production : {
-      NODE_ENV: 'production'
+    {
+      name      : 'node-image-farmer',
+      script    : 'node-image-farmer/app/app.js',
+      watch     : true,
+      env: {
+        NODE_ENV: 'development',
+        PORT: 3002
+      },
+      env_production : {
+        NODE_ENV: 'production',
+        PORT: 3002
+      }
     }
-  }]
+  ]
 };
